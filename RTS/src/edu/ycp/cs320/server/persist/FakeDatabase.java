5a084eeb285440391a58bdddc76aa3e14b988455
Dan Mashuda <dmashuda@ycp.edu>
2014-04-23 11:31:56 -0400

    updated FakeDatabase functions

--- RTS/src/edu/ycp/cs320/server/persist/FakeDatabase.java
+++ RTS/src/edu/ycp/cs320/server/persist/FakeDatabase.java
@@ -1,19 +1,40 @@
 package edu.ycp.cs320.server.persist;
 
+import java.util.ArrayList;
+
 import edu.ycp.cs320.rts.shared.UserData;
 
 public class FakeDatabase implements IDatabase {
-
+	
+	private ArrayList<UserData> users;
+	
+	public FakeDatabase() {
+		users = new ArrayList<UserData>();
+	}
 	@Override
 	public boolean createUser(UserData user) {
-		// TODO Auto-generated method stub
-		return false;
+		boolean check = users.add(user);
+		return check;
 	}
 
 	@Override
 	public String Validatelogin(String username, String password) {
-		// TODO Auto-generated method stub
+		
+		for(UserData d: users){
+			if(d.getUsername().equals(username)){
+				
+				//checks to see if the plain text password is a match to the hash in the database
+				boolean check = BCrypt.checkpw(password, d.getPasswordhash());
+				if(check){
+					// if true, the function returns the username
+					return d.getUsername();
+				}
+			}
+		}
+		//if false, it returns null
 		return null;
 	}
+
+	
 
 }
