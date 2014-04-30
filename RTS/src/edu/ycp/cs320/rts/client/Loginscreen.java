4c457ef41ca776d76400f8ccbdb6f1cb143ae90f
Dan Mashuda <dmashuda@ycp.edu>
2014-04-29 22:34:40 -0400

    Added Login implementation for client and server side

--- RTS/src/edu/ycp/cs320/rts/client/Loginscreen.java
+++ RTS/src/edu/ycp/cs320/rts/client/Loginscreen.java
@@ -1,10 +1,21 @@
 package edu.ycp.cs320.rts.client;
 
+
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.Timer;
+import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.PasswordTextBox;
+import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.TextBox;
+import com.google.gwt.user.client.ui.VerticalPanel;
 
+import edu.ycp.cs320.rts.server.control.AddUser;
+import edu.ycp.cs320.rts.shared.UserData;
 import edu.ycp.cs320.rts.shared.GameState;
 
 public class Loginscreen implements EntryPoint {
@@ -13,7 +24,7 @@ public class Loginscreen implements EntryPoint {
 	public void onModuleLoad() {
 		
 		final GetBoardServiceAsync boardservice = (GetBoardServiceAsync) GWT.create(GetBoardService.class);
-		AsyncCallback<Boolean> callbackcreate = new AsyncCallback<Boolean>() {
+		final AsyncCallback<Boolean> callbackcreate = new AsyncCallback<Boolean>() {
 
 			@Override
 			public void onFailure(Throwable caught) {
@@ -23,7 +34,6 @@ public class Loginscreen implements EntryPoint {
 
 			@Override
 			public void onSuccess(Boolean result) {
-				GWT.log("Action Success");
 				if(result){
 					GWT.log("Action returned true");
 					
@@ -46,14 +56,13 @@ public class Loginscreen implements EntryPoint {
 				GWT.log("Action Success");
 				if(result){
 					GWT.log("Action returned true");
-					
 				}
 				
 			}
 			
 		};
 		
-		boardservice.newuser("dmashuda", "abc123", "dmashuda@ycp.edu", callbackcreate);
+		//boardservice.newuser("dmashuda", "abc123", "dmashuda@ycp.edu", callbackcreate);
 		
 		Timer t = new Timer() {
 			
@@ -64,7 +73,101 @@ public class Loginscreen implements EntryPoint {
 			}
 		};
 		
-		t.scheduleRepeating(1000);
+		//t.scheduleRepeating(1000);
+		
+
+		final TextBox usernamebox = new TextBox();
+		usernamebox.setTitle("Username");
+		usernamebox.setText("username");
+		final PasswordTextBox passwordbox = new PasswordTextBox();
+		passwordbox.setTitle("password");
+		Button loginbtn = new Button();
+		loginbtn.setText("Login");
+		Button switchtocreate = new Button();
+		switchtocreate.setText("No Account?");
+		final VerticalPanel loginpanel = new VerticalPanel();
+		final VerticalPanel createuserpanel = new VerticalPanel();
+		
+		final TextBox usertext = new TextBox();
+		usertext.setText("username");
+		usertext.setTitle("username");
+		final PasswordTextBox pword1 = new PasswordTextBox();
+		final PasswordTextBox pword2 = new PasswordTextBox();
+		final TextBox email = new TextBox();
+		email.setText("email");
+		email.setTitle("email");
+		
+		Button createuserbtn = new Button();
+		Button switchtologinbtn = new Button();
+		switchtologinbtn.setText("Switch to login");
+		createuserbtn.setText("Create Account");
+		
+		switchtologinbtn.addClickHandler(new ClickHandler() {
+			
+			@Override
+			public void onClick(ClickEvent event) {
+				RootPanel.get().remove(createuserpanel);
+				RootPanel.get().add(loginpanel);
+				
+			}
+		});
+		
+		createuserbtn.addClickHandler(new ClickHandler() {
+			
+			@Override
+			public void onClick(ClickEvent event) {
+				
+				;
+				if(pword1.getText().toString().equals(pword2.getText().toString())){
+					boardservice.newuser(usertext.getText().toString(), pword1.getText().toString(), email.getText().toString(), callbackcreate);
+				}
+				else{
+					Window.alert("passwords do not match");
+				}
+				
+				
+			}
+		});
+		
+		createuserpanel.add(usertext);
+		createuserpanel.add(pword1);
+		createuserpanel.add(pword2);
+		createuserpanel.add(email);
+		createuserpanel.add(createuserbtn);
+		createuserpanel.add(switchtologinbtn);
+		
+		
+		
+		
+		
+		switchtocreate.addClickHandler(new ClickHandler() {
+			
+			@Override
+			public void onClick(ClickEvent event) {
+				RootPanel.get().remove(loginpanel);
+				RootPanel.get().add(createuserpanel);
+				
+			}
+		});
+		
+		loginbtn.addClickHandler(new ClickHandler() {
+			
+			@Override
+			public void onClick(ClickEvent event) {
+				// TODO Auto-generated method stub
+				boardservice.login(usernamebox.getText().toString(), passwordbox.getText().toString(), callbacklogin);
+				
+			}
+		});
+		
+		
+		
+		loginpanel.add(usernamebox);
+		loginpanel.add(passwordbox);
+		loginpanel.add(loginbtn);
+		loginpanel.add(switchtocreate);
+		
+		RootPanel.get().add(loginpanel);
 		
 
 	}
